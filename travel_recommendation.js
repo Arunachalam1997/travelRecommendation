8c83b146eb6d212e62f9cab57dc8fa45cfb30046
ArunJaisankar <104357654+ArunJaisankar@users.noreply.github.com>
2024-05-07 17:34:45 +0530

    css update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,11 @@
 const searchButton = document.getElementById('btnSearch');
+const clearbtn = document.getElementById('btnClear');
+const resultsDiv = document.getElementById('results');
+
+
+function clearInput(){
+     document.getElementById('searchInput').value = '';
+}
 
 function searchResult(){
     const searchInput = document.getElementById('searchInput').value;
@@ -6,11 +13,13 @@ function searchResult(){
     .then(response => response.json())
     .then(data => {
         const results = data.countries.find(item => item.name == searchInput )
-
+ 
         if(results){
-            
+            const cityName = results.city.name;
+            console.log(cityName);
         }
     })
 }
 searchResult();
 searchButton.addEventListener('click', searchResult);
+clearbtn.addEventListener('click', clearInput);
